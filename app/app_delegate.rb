1a441d2f9b46f123f44d8655707cea103f5f0091
Andrew Nesbitt <andrewnez@gmail.com>
2012-05-12 20:54:07 +0100

    Black status bar

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,5 +1,6 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+    application.setStatusBarStyle(UIStatusBarStyleBlackOpaque)
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
     @window.rootViewController = TimerController.alloc.init
     @window.rootViewController.wantsFullScreenLayout = true
