091ddd1e2f1eac5424cc573ea6d8f799c6a0db68
Andrew Nesbitt <andrewnez@gmail.com>
2012-10-22 13:45:33 +0100

    Don't let the device go to sleep

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,6 +1,7 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
     application.setStatusBarStyle(UIStatusBarStyleBlackOpaque)
+    application.idleTimerDisabled = true
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
     @window.rootViewController = TimerController.alloc.init
     @window.rootViewController.wantsFullScreenLayout = true
