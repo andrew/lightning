9f8e30356bb8aee1487aa6a96d79268d18870518
Andrew Nesbitt <andrewnez@gmail.com>
2012-10-24 09:14:01 +0100

    Re-write

--- app/preferences_controller.rb
+++ app/preferences_controller.rb
@@ -1,9 +1,25 @@
 class PreferencesController < UIViewController
-  def loadView
-    self.view = PreferencesView.alloc.init
-  end
+  def viewDidLoad
+    super
+    self.view.addSubview @label
 
-  def shouldAutorotateToInterfaceOrientation(*)
-    true
+    self.view.backgroundColor = UIColor.colorWithRed(255, green:255, blue:255, alpha:0.8  )
+
+    @action = UIButton.buttonWithType(UIButtonTypeRoundedRect)
+    @action.setTitle('Save', forState:UIControlStateNormal)
+    @action.addTarget(self, action:'actionTapped', forControlEvents:UIControlEventTouchUpInside)
+    @action.frame = [[20, 300], [view.frame.size.width - 20 * 2, 40]]
+    self.view.addSubview(@action)
+
+    @label = UILabel.alloc.initWithFrame(CGRectZero)
+    @label.text = "Trolololo"
+    @label.sizeToFit
+    @label.backgroundColor = UIColor.colorWithRed(255, green:255, blue:255, alpha:0)
+    @label.center = CGPointMake(self.view.frame.size.width / 2, self.view.frame.size.height / 2)
+    self.view.addSubview @label
+  end
+  
+  def actionTapped
+    dismissModalViewControllerAnimated(true)
   end
 end
--- app/timer_controller.rb
+++ app/timer_controller.rb
@@ -1,16 +1,136 @@
 class TimerController < UIViewController
   def loadView
-    self.view = TimerView.alloc.init
+    self.view = UIView.new
+  end
+
+  def viewDidLoad
+    @paused = true
+    @duration = default_time
+
+    initialize_label
+    update_label
+    initalize_pref_button
+  end
+
+  def initialize_label
+    constrain = CGSize.new(view.frame.size.width, view.frame.size.height)
+    size = label_text.sizeWithFont(font, constrainedToSize:constrain)
+
+    x = (view.frame.size.width - size.width)/2
+    y = (view.frame.size.height - size.height)/2
+
+    @label = UILabel.alloc.initWithFrame([[x,y], [size.width, view.frame.size.height]])
+    @label.font = font
+    @label.userInteractionEnabled = true
+    @label.center = CGPointMake(self.view.frame.size.width / 2, self.view.frame.size.height / 2)
+    @label.addGestureRecognizer(double_tap)
+    @label.addGestureRecognizer(single_tap)
+
+    self.view.addSubview @label
+  end
+  
+  def initalize_pref_button
+    @pref_button = UIButton.buttonWithType(UIButtonTypeInfoLight)
+    @pref_button.addTarget(self, action:'showPreferences', forControlEvents:UIControlEventTouchUpInside)
+    @pref_button.frame = [[view.frame.size.width - 40, view.frame.size.height - 40], [20, 20]]
+    
+    self.view.addSubview(@pref_button)
+  end
+  
+  def label_text
+    Time.at(@duration).gmtime.strftime('%M:%S')
+  end
+  
+  def font
+    font_size = view.frame.size.width/3
+    UIFont.boldSystemFontOfSize(font_size)
+  end
+  
+  def background_color
+    percentage = 1 - (@duration/default_time)
+    UIColor.colorWithRed(percentage, green:0, blue:0, alpha:1.0)
+  end
+  
+  def label_color
+    UIColor.colorWithRed(1, green:1, blue:1, alpha:(@paused ? 0.3 : 1.0))
+  end
+  
+  def single_tap
+    tap = UITapGestureRecognizer.alloc.initWithTarget(self, action: :'tapped:')
+    tap.requireGestureRecognizerToFail(double_tap)
+    return tap
+  end
+  
+  def double_tap
+    doubletap = UITapGestureRecognizer.alloc.initWithTarget(self, action: :'reset:')
+    doubletap.numberOfTapsRequired = 2
+    return doubletap
+  end
+  
+  def update_label
+    self.view.backgroundColor = background_color
+    @label.text = label_text
+    @label.backgroundColor = background_color
+    @label.color = label_color
+  end
+
+  def tapped(sender)
+    if @timer || @duration < 0.1
+      @duration = default_time if @duration < 0.1
+      pause
+    else
+      @paused = false
+      @timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target:self, selector:'timerFired', userInfo:nil, repeats:true)
+      update_label
+    end
+  end
+
+  def reset(sender)
+    @duration = default_time
+    pause
   end
 
   def shouldAutorotateToInterfaceOrientation(*)
     true
-    # showPreferences
+  end
+
+  def default_time
+    5*60.0
+  end
+
+  def removeTimer
+    @timer.invalidate unless @timer.nil?
+    @timer = nil
+  end
+
+  def timerFired
+    if @duration < 0.1
+      time_out
+    else
+      @duration -= 0.1
+      update_label
+    end
+  end
+  
+  def pause
+    @paused = true
+    removeTimer
+    update_label
+  end
+
+  def time_out
+    removeTimer
+    play_buzzer
+  end
+
+  def play_buzzer
+    BW::Media.play(NSURL.fileURLWithPath(File.join(NSBundle.mainBundle.resourcePath, 'buzzer.wav'))) {}
   end
   
   def showPreferences
-    controller = UIApplication.sharedApplication.delegate.preferences_controller
-    navigationController = UINavigationController.alloc.initWithRootViewController(controller)
-    navigationController.pushViewController(controller, animated:true)
+    pause
+    new_controller = PreferencesController.alloc.init
+    new_controller.modalTransitionStyle = UIModalTransitionStyleFlipHorizontal
+    self.presentModalViewController(new_controller, animated: true)
   end
 end
